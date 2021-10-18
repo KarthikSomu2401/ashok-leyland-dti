2f2ef634b2e79e3ef2277e2c02a5f11daabb486e
Karthikeyan Somasundaram <karthikitmsp@gmail.com>
2021-10-18 11:08:32 +0100

    sensor crossed post to get

--- backend/routes/test.route.js
+++ backend/routes/test.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 let test_controller = require("../controllers/test.controller");
 
-router.route("/sensorCrossed").post(test_controller.test_sensor_data);
+router.route("/sensorCrossed").get(test_controller.test_sensor_data);
 router.route("/testStatus").post(test_controller.test_status);
 
 module.exports = router;
